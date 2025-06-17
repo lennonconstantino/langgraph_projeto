3cce345d58e8530d52a1f801064b2631991fcffb
lennonconstantino <lennonconstantino@gmail.com>
2025-06-16 21:20:22 -0300

    creating queries

--- graph.py
+++ graph.py
@@ -9,6 +9,8 @@ from prompts import *
 from dotenv import load_dotenv
 import os
 
+from tavily import TavilyClient
+
 load_dotenv()
 OPEN_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -18,9 +20,23 @@ reasoning_llm = ChatOpenAI(model="o4-mini", api_key=OPEN_API_KEY)
 
 # Nos
 def build_first_queries(state: ReportState):
+    class QueryList(BaseModel):
+        queries: List[str]
+
     user_input = state.user_input
     prompt =  build_queries.format(user_input=user_input)
-    return {""}
+    query_llm = llm.with_structured_output(QueryList)
+    result = query_llm.invoke(prompt)
+    return {"queries":result.queries}
+
+def search_tavily(query: str):
+    tavily_client = TavilyClient()
+    results = tavily_client.search(query, max_results=1, include_raw_content=False)
+    url = results["results"][0]["url"]
+    url_extraction = tavily_client.extract(url)
+    if (len(url_extraction["results"])>0):
+        raw_content = url_extraction["results"][0]["raw_content"]
+
 
 # Edges
 
