7ba8602933dd52e6b78b1d87e378b557cd1c0cf4
lennonconstantino <lennonconstantino@gmail.com>
2025-06-16 21:07:40 -0300

    define models

---
+++ graph.py
@@ -0,0 +1,36 @@
+from pyasn1_modules.rfc2315 import EncryptedDigest
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langgraph.graph import START, END, StateGraph
+from langgraph. types import Send
+
+from schemas import *
+from prompts import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+OPEN_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# Modelos
+llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=OPEN_API_KEY)
+reasoning_llm = ChatOpenAI(model="o4-mini", api_key=OPEN_API_KEY)
+
+# Nos
+
+
+# Edges
+
+
+# Grafo
+builder = StateGraph(ReportState)
+graph = builder.compile()
+
+# execuçao
+if __name__ == "__main__":
+    user_input = """
+    Quero que você me explicque o processo
+    total para construir um agente de IA
+    """
+    graph.invoke({"user_input": user_input})
+
